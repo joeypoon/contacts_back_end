0ebaa8e62da49a9359f0e8c85ac576688d7dbb3c
Joey Poon <joey@joeypoon.com>
2015-07-02 20:09:04 -0500

    add create user and merge to master

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,22 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find params[:id] 
+    @user = User.find params[:id]
   end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      render :show, status: 200
+    else
+      render json: { error: @user.errors }, status: 422
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :phone, :company, :city, :state, :facebook, :twitter, :linkedin, :github, :instagram)
+    end
 
 end
