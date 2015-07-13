a01f4ffd8b8abf3f9bf141c2380c089e22dfdd68
Martin Bee <martbee88@gmail.com>
2015-07-13 14:33:23 -0500

    removed email validation from contact model

--- app/models/contact_info.rb
+++ app/models/contact_info.rb
@@ -1,9 +1,5 @@
 class ContactInfo < ActiveRecord::Base
-  
-  belongs_to :user
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  belongs_to :user
 
 end
