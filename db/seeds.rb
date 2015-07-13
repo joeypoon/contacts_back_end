0d951b270cc6203503b9a49e39f8acea3a3d6d7a
Martin Bee <martbee88@gmail.com>
2015-07-13 14:30:59 -0500

    modified seeds so that they fit the new user model with names and email

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
   lat = [-58.000001, -58.000002, -58.000003, -58.000004, 100.0000000 ].sample
   lng = [-68.000001, -68.000002, -68.000003, -68.000004, 100.0000000].sample
 
-  user = User.create! password: 'password', password_confirmation: 'password', lat: lat, lng: lng
-  ContactInfo.create! name: Faker::Name.name, email: Faker::Internet.email, user_id: user.id
+  user = User.create! password: 'password', password_confirmation: 'password', email: Faker::Internet.email, name: Faker::Name.name, lat: lat, lng: lng
+  ContactInfo.create! user_id: user.id
 
 end
