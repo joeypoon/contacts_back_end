5d1bce695bc8e3f11b043dba2efecccac7625341
Martin Bee <martbee88@gmail.com>
2015-07-13 11:28:30 -0500

    added profile.json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
     @user.create_contact_info contact_params
     if @user.save
-      render :show, status: 201
+      render :profile, status: 201
     else
       render json: { error: @user.errors }, status: 422
     end
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     contact = ContactInfo.find_by email: user_params[:email]
     @user = User.find (contact.id)
     if @user.authenticate(user_params[:password])
-      render :show
+      render :login, status: 200
     else
       render json: { error: "incorrect email/password combination" }, status: 422
     end
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find params[:id]
     if @user.update user_params
-      render :show, status: 200
+      render :profile, status: 200
     else
       render json: { error: @user.errors }, status: 422
     end
