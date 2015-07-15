ae7b0d57a2ecabc985da707903d2e338db520434
Joey Poon <joey@joeypoon.com>
2015-07-15 11:16:00 -0500

    remove contact info table and move columns to users table

---
+++ db/migrate/20150715161255_remove_in_outbound_and_contact_list_from_users.rb
@@ -0,0 +1,7 @@
+class RemoveInOutboundAndContactListFromUsers < ActiveRecord::Migration
+  def change
+    remove_column :users, :inbound_shares, :json
+    remove_column :users, :outbound_shares, :json
+    remove_column :users, :contact_list, :integer
+  end
+end
