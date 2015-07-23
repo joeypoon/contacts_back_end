d6695b51130de9a412aa4ab470ce3e6832b5af05
Martin Bee <martbee88@gmail.com>
2015-07-23 17:12:17 -0500

    seriously, save contact_list in contact delete action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -113,8 +113,7 @@ class UsersController < ApplicationController
 
   def destroy_contact
     current_user = User.find(params[:id])
-    user_id = params[:user_id]
-    current_user.contact_list.list.delete(id: params[:user_id])
+    current_user.contact_list.list.delete(19)
     current_user.contact_list.save
     contact_ids = current_user.contact_list.list
     @users = User.where(id: contact_ids)
