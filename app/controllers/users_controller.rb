90012abdd968948bdac3c529492f937cb741070b
Martin Bee <martbee88@gmail.com>
2015-07-23 15:27:16 -0500

    added save to delete contacts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -108,6 +108,8 @@ class UsersController < ApplicationController
   def destroy_contact
     current_user.contact_list.list.delete(params[:user_id])
     if current_user.contact_list.save
+      contact_ids = current_user.contact_list.list
+      @users = User.where(id: contact_ids)
       render :contacts, status: 200
     else
       render json: { error: "You have failed to achieve victory" }, status: 422
