523e50bee6ea04da870fb74db8f471a998ac0717
Martin Bee <martbee88@gmail.com>
2015-07-24 09:32:19 -0500

    fixed batman image

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -19,7 +19,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
 
   def default_url
-     "https://s3.amazonaws.com/contacts-bee/default.png"
+     "https://s3.amazonaws.com/contacts-bee/batman.png"
   end
 
   # Process files as they are uploaded:
