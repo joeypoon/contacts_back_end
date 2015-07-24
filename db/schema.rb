3df7973a574ba3a3662d264882167742e7965782
Martin Bee <martbee88@gmail.com>
2015-07-23 21:29:58 -0500

    added skype

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150716171027) do
+ActiveRecord::Schema.define(version: 20150724005819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20150716171027) do
     t.string   "linkedin"
     t.string   "twitter"
     t.string   "site"
+    t.string   "skype"
   end
 
 end
