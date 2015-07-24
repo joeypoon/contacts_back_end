d14242e97db42ef26380a1ba3250c89d3d1cee27
Martin Bee <martbee88@gmail.com>
2015-07-23 21:18:18 -0500

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,9 @@
 #   ContactList.create! user_id: user.id
 #
 # end
+
+paul = User.create! (name:"Paul Saad", email:"paulesaad@gmail.com", company:"React Natives", phone:"1234567890", site:"paulesaad.com", twitter:"@paulesaad", github:"/paulesaad", instagram:"@paulesaad", facebook:"/paulesaad", linkedin:"paul saad", skype:"paulesaad", password:"password", password_confirmation:"password", avatar:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAIxAAAAJGQyMGZmNWE0LWExYmMtNDQ3OS04YTViLWEyNmY2ZWFlZjliNw.jpg")
+
+joey = User.create! (name:"Joey Poon", email:"joeypoon@gmail.com", company:"Erdos Miller", phone:"1234567890", site:"joeypoon.com", twitter:"@joeypoon", github:"/joeypoon", instagram:"@joeypoon", facebook:"/joeypoon", linkedin:"joey poon", skype:"joeypoon", password:"password", password_confirmation:"password", avatar:"https://avatars2.githubusercontent.com/u/11009772?v=3&s=460")
+
+martin = User.create! (name:"Martin Bee", email:"martbee@gmail.com", company:"The Iron Yard", phone:"1234567890", site:"martbee.com", twitter:"@martbee", github:"/martbee", instagram:"@martbee", facebook:"/martbee", linkedin:"paul saad", skype:"martbee", password:"password", password_confirmation:"password", avatar:"https://avatars2.githubusercontent.com/u/9852415?v=3&s=460")
